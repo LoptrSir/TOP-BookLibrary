7f72fcaad365b06d6f5cb529300509963097a6ba
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-08-10 14:08:25 -0600

    Read button functions

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// ToP JS Course> Objects and Object constructors>  Book exercise //
+// ToP JS Course> Objects and Object constructors>  Book exercise // Aug 2023
 
-// 8/7/23 this iteration works. No CSS done yet. Correct LibraryDisplay to only add new book not generate a completely new display.  Read y/n does'nt update to has read/has not read.
+// TO WORK ON: ReadBook button to display- ReadBook function- Delete function-  CSS Styling
 
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: "HardCode",
+    author: "Sir Not Appearing",
+    pages: "infinite",
+    read: false,
+  },
+];
 
 function book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read.toLowerCase() === "y"; // Modify this to reject !y/n input
-  this.info = `${title} by ${author}, ${pages} pages, ${
-    this.read ? "this book is read." : "this book is unread."
-  }`;
-
+  this.read = read.toLowerCase() === "y";
   console.log("book-", this.info);
 }
 
@@ -21,99 +24,79 @@ function addBookToLibrary() {
   const authorInput = document.getElementById("author");
   const pagesInput = document.getElementById("pages");
   const readInput = document.getElementById("read");
-
-  const title = titleInput.value;
-  const author = authorInput.value;
-  const pages = pagesInput.value;
-  const readValue = readInput.value;
+  const title = titleInput.value;     // * this section can be cleaned up with a .map() section
+  const author = authorInput.value;   //*
+  const pages = pagesInput.value;     //*
+  const readValue = readInput.value;  //*
 
   if (title && author && pages && readValue) {
     const newBook = new book(title, author, pages, readValue);
     myLibrary.push(newBook);
-    // clear Input.value here or clear from page reset button?
-    titleInput.value = "";
-    authorInput.value = "";
-    pagesInput.value = "";
-    readInput.value = "";
+    titleInput.value = "";            //*
+    authorInput.value = "";           //*
+    pagesInput.value = "";            //*
+    readInput.value = "";             //*  see below;
+    // 
+    //             //           const inputElements = [
+    //     document.getElementById("title"),
+    //     document.getElementById("author"),
+    //     document.getElementById("pages"),
+    //     document.getElementById("read"),
+    //             //            ];
+    
+    //   const values = inputElements.map((input) => {
+    //     const value = input.value;
+    //     input.value = ""; // Clear the input value
+    //     return value;
+    //   });
+    
+    //   const [title, author, pages, readValue] = values;
+
+
     console.log("abtl-", title, author, pages, readValue);
 
     displayLibrary();
   }
 }
 
-// SUGGESTED CODE2 BEGIN
 function displayLibrary() {
-  // Remove any existing library containers
   const existingLibraries = document.querySelectorAll("div.library");
   existingLibraries.forEach((existingLibrary) => {
     existingLibrary.remove();
   });
 
-  // Create a new library container
   const libraryContainer = document.createElement("div");
-  libraryContainer.className = "library"; // Apply the class "library"
+  libraryContainer.className = "library";
   libraryContainer.innerText = "Library";
-  //libraryContainer.innerHTML = "<h3>Library</h3>";  XSS vulnerability
 
-  // Populate the new library container with book details
   myLibrary.forEach((book, index) => {
-    const bookDetails = document.createElement("p");
+    // const bookDetails = document.createElement("p"); modifying below
+    const bookDetails = document.createElement("div"); // do I need a class here for css styling, I assume yes.
 
-    const delBook = document.createElement('button');
-    delBook.innerText = 'Delete Book';
+    const delBook = document.createElement("button");
+    delBook.innerText = "Delete Book";
+    // insert eventListener here for click
+    // if (book.read === false) { modify below
+    bookDetails.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read ? "this book is read." : "this book is unread."}`;
 
-    const readBook = document.createElement('button');
-    readBook.innerText = 'Read Book';
+    if (!book.read) {
+      const readBook = document.createElement("button");
+      readBook.innerText = "Read Book";
+      // insert addEventListener here
+      bookDetails.appendChild(readBook);
+    }
+    console.log("br-", book.read);
 
-    bookDetails.textContent = `${book.info}`;
+    // bookDetails.textContent = // `${book.info}`; //Modified below
+    //bookDetails.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read ? "this book is read." : "this book is unread."}`;
+
     bookDetails.appendChild(delBook);
-    bookDetails.appendChild(readBook);
     libraryContainer.appendChild(bookDetails);
   });
 
-  // Append the new library container to the DOM
   document.body.appendChild(libraryContainer);
 }
-// SUGGESTED CODE2 END
+displayLibrary(); // needed to call hardcoded library
 
-// // suggested code1 Begin:
-// function displayLibrary() {
-//   // Clear previous library container content
-//   const existingLibrary = document.querySelector("div.library");
-//   if (existingLibrary) {
-//     existingLibrary.innerHTML = ''; // Clear the content of the existing library  }
-//   // Create a new library container
-//   const libraryContainer = document.createElement("div");
-//   libraryContainer.innerHTML = "<h3>Library</h3>";
-//   // Populate the new library container with book details
-//   myLibrary.forEach((book, index) => {
-//     const bookDetails = document.createElement("p");
-//     bookDetails.textContent = `Book ${index + 1}: Title- ${
-//       book.title}, Author- ${book.author}, Pages- ${book.pages}, Read- ${book.read}`;
-//     libraryContainer.appendChild(bookDetails);  });
-//   // Append the new library container to the DOM
-//   if (existingLibrary) {
-//     existingLibrary.appendChild(libraryContainer);  } else {
-//     document.body.appendChild(libraryContainer); }}
-// // SUGGESTED CODE1 END:
-
-// // Original code block Begin
-// const libraryContainer = document.createElement("div");
-//  libraryContainer.innerHTML = "<h3>Library</h3>";
-// function displayLibrary() {
-//   myLibrary.forEach((book, index) => {
-//    const bookContainer = document.createElement("div");
-//    const bookDetails = document.createElement("p");
-//    bookDetails.textContent = `${book.info}`;
-//    bookContainer.appendChild(bookDetails);
-//    //  bookContainer.appendChild(deleteButton);
-//    //  bookContainer.appendChild(ToggleReadButton);  //only visible if read == n
-//    libraryContainer.appendChild(bookContainer);
-//   });
-//   const existingLibrary = document.querySelector("div.library");
-//   if (existingLibrary) {
-//    // append only next book to existing display
-//    existingLibrary.replaceWith(libraryContainer);
-//    //existingLibrary.appendChild(libraryContainer);  // doesn't stop recreation of entire library
-//   } else { document.body.appendChild(libraryContainer);}}
-// //Original code block End
+//function deleteBook(index) { }
+// function bookToRead(){ }
